1c571facdd9aea45f7ec33d55772eca5ae5f6fa2
Mikeychocho <mikeychocho@gmail.com>
2023-07-20 23:51:40 +0100

    removed __pycache__ folders from git

--- CourseApp/settings.py
+++ CourseApp/settings.py
@@ -113,13 +113,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+# Static Files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_URL = 'static/'
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
