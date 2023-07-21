50413d226a2993664370183f2390e01ba49d2565
Mikeychocho <mikeychocho@gmail.com>
2023-07-21 05:22:12 +0100

    initiated and create files for admin panel...

--- all_apps/admin_pages/urls.py
+++ all_apps/admin_pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.CreateCourse.as_view(), name='create'),
-    path('edit/', views.EditCourse.as_view(), name='edit'),
+    path('admin-panel/', views.AdminPage.as_view(), name='admin'),
+    path('create/', views.CreateCourse.as_view(), name='create_course'),
+    path('edit/', views.EditCourse.as_view(), name='edit_course'),
 ]
--- all_apps/admin_pages/views.py
+++ all_apps/admin_pages/views.py
@@ -4,6 +4,21 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from .models import *
 
+
+
+class AdminPage(View):
+    def __init__(self):
+        self.template_name = 'admin.html'
+
+    @method_decorator(login_required(login_url='signup'))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+
+        return render(request, self.template_name)
+
+
 class CreateCourse(View):
     def __init__(self):
         self.template_name = 'course/course_create.html'
