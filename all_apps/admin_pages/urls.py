8f725f0c499acb9b9b78b7edaffdec969148c65a
Mikeychocho <mikeychocho@gmail.com>
2023-07-21 05:45:38 +0100

    refactor the code from my flask to djanngo

--- all_apps/admin_pages/urls.py
+++ all_apps/admin_pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin-panel/', views.AdminPage.as_view(), name='admin'),
+    path('panel/', views.AdminPage.as_view(), name='admin'),
     path('create/', views.CreateCourse.as_view(), name='create_course'),
     path('edit/', views.EditCourse.as_view(), name='edit_course'),
 ]
